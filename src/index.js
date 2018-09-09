ab5d65153e5b6a692e5e07ecba3ed15ebf0e6f16
Mats Krüger Svensson <mats@matsemann.com>
2018-09-09 16:41:31 +0200

    three.js scene

--- src/index.js
+++ src/index.js
@@ -1,8 +1,55 @@
-const intersector = require('./intersector.js');
+import * as THREE from 'three';
+import * as OrbitControlsSetup from 'three-orbit-controls';
 
-console.log(intersector.findLineIntersection(
-    {x: 0, y: 0},
-    {x: 10, y: 10},
-    {x: 10, y: 5},
-    {x: 9, y: 5}
-));
+const OrbitControls = OrbitControlsSetup(THREE);
+
+console.log("running2");
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+var light = new THREE.AmbientLight( 0xffffff ); // soft white light
+scene.add( light );
+
+var pointLight = new THREE.PointLight( 0xffffff, 1, 0 );
+pointLight.position.set(0, 5, 0);
+scene.add(pointLight);
+var pointLight2 = new THREE.PointLight( 0xffffff, 1, 0 );
+pointLight2.position.set(0, 5, 5);
+scene.add(pointLight2);
+var pointLight3 = new THREE.PointLight( 0xffffff, 1, 0 );
+pointLight3.position.set(3, 5, -5);
+scene.add(pointLight3);
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshPhongMaterial( {
+    color: 0x555555,
+    // specular: 0xffffff,
+    // shininess: 50
+} );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+
+
+const controls = new OrbitControls( camera );
+// controls.autoRotate = true;
+camera.position.z = 5;
+controls.update();
+
+
+var animate = function () {
+    requestAnimationFrame( animate );
+
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
+    controls.update();
+
+    renderer.render( scene, camera );
+};
+
+animate();
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,5 +1,7 @@
+const path = require('path');
 
 module.exports = {
     watch: true,
-    mode: "development"
+    mode: "development",
+    devtool: "eval-source-map"
 };

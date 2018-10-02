848a44a5ffa73e22a9ed8d83c4d63e04f07694be
Mats Krüger Svensson <mats@matsemann.com>
2018-10-02 17:11:52 +0200

    ahhhhh, have to intersect same halves for it to not flip axis 🤯

--- src/ImpossibleCalculator.js
+++ src/ImpossibleCalculator.js
@@ -76,7 +76,7 @@ export function findPoints(view1, view2, func1, func2, numPoints) {
         const func1x = func1(xVal);
         const func2x = func2(xVal);
 
-        const intersection2d = findLineIntersection({x: -view1.z, y: view1.y}, {x: 0, y: func1x}, {x: -view2.z, y: view2.y}, {x: 0, y: func2x});
+        const intersection2d = findLineIntersection({x: -view1.z, y: view1.y}, {x: func1x, y: 0}, {x: -view2.z, y: view2.y}, {x: func2x, y: 0});
         intersections.push({x: xVal, y: intersection2d.y, z: -intersection2d.x});
     }
 
--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ import { findPoints, halfCircle, halfStar, inverse, shift, triangle, wave } from
 const myRenderer = new Renderer(window.innerWidth, window.innerHeight, window.devicePixelRatio);
 document.body.appendChild( myRenderer.renderer.domElement );
 
-// const intersections = findPoints({x: 0, y:50, z:50}, {x: 0, y: 50, z: -50}, shift(triangle, -1), shift(halfCircle,0), 50);
-// const intersections2 = findPoints({x: 0, y:50, z:50}, {x: 0, y: 50, z: -50}, inverse(shift(triangle, -1)), inverse(halfCircle), 50);
+const intersections = findPoints({x: 0, y:50, z:50}, {x: 0, y: 50, z: -50}, inverse(shift(triangle, -1)), shift(halfCircle,0), 200);
+const intersections2 = findPoints({x: 0, y:50, z:50}, {x: 0, y: 50, z: -50}, shift(triangle, -1), inverse(halfCircle), 200);
 
 // const intersections = findPoints({x: 0, y:50, z:50}, {x: 0, y: 50, z: -50}, inverse(halfCircle), shift(halfStar,0), 100);
 // const intersections2 = findPoints({x: 0, y:50, z:50}, {x: 0, y: 50, z: -50}, shift(halfCircle, 0), inverse(halfStar), 100);
-myRenderer.renderPoints(debugFunction(halfStar, 100));
-myRenderer.renderPoints(debugFunction(inverse(halfStar), 100));
-// myRenderer.renderPoints(intersections);
-// myRenderer.renderPoints(intersections2);
+// myRenderer.renderPoints(debugFunction(halfStar, 100));
+// myRenderer.renderPoints(debugFunction(inverse(halfStar), 100));
+myRenderer.renderPoints(intersections);
+myRenderer.renderPoints(intersections2);
 
 window.addEventListener('resize', () => {
     console.log("Resizing");
